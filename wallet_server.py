42b29bd4a5b6532880e0d2a6522a7ba5e0655e4e
EggPool <33421807+EggPool@users.noreply.github.com>
2018-06-10 16:58:08 +0200

    Add updates
    Lost @hclivess updates in the process, re-introduced.

--- wallet_server.py
+++ wallet_server.py
@@ -70,13 +70,13 @@ class WalletServer(TCPServer):
             try:
                 await self.command(stream, ip)
             except StreamClosedError:
-                access_log.info("Client " + str(address) + " left.")
+                access_log.info("Client {} left.".format(address))
                 error_shown = True
                 WalletServer.clients.remove(address)
                 break
             except ValueError:
                 if options.verbose:
-                    access_log.info("Client " + str(address) + " Rejected.")
+                    access_log.info("Client {} Rejected.".format(address))
                 error_shown = True
                 WalletServer.clients.remove(address)
                 break
@@ -439,7 +439,7 @@ def start_server(port):
     server.bind(port)
     server.start(1)  # Force one process only
     if options.verbose:
-        app_log.info("Starting server on tcp://localhost:" + str(port))
+        app_log.info("Starting server on tcp://localhost:{}".format(port))
     io_loop = IOLoop.instance()
     io_loop.spawn_callback(server.background)
     try:
@@ -518,8 +518,6 @@ if __name__ == "__main__":
             sys.exit()
     else:
         process = None
-
-
 
     app_log.info("Wallet Server {} Starting.".format(__version__))
 
